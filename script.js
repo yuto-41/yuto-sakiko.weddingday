c87ea64323c635bfb1ebae6a0170d64fdd6ab931
yuto-41 <youtobe.1997.01.20@gmail.com>
2026-08-20 14:14:57 +0900

    js修正

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeSuccessButton = document.getElementById('closeSuccessButton');
     
     // GAS WebアプリのURL
-    const gasUrl = 'https://script.google.com/macros/s/AKfycbznc7XgFOUf8hkwGqATDbkAtQiexXEv8JO-C5ToFpDQOgPzEzadXhKtogZG2VOwdcoU/exec';
+    const gasUrl = 'https://script.google.com/macros/s/AKfycbxhCHYKaaerZ9k8k9lkMAx1fkk_B7GmlYXGzQjbtmudUwv6xwWHVJ8ncgiGbmtFr6St/exec';
     
     if (submitButton && rsvpForm) {
         submitButton.addEventListener('click', async function(e) {
